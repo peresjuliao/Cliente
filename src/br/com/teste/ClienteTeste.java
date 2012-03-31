faa6a7ba0679d8f7df4560bd7bffc79c1b47d7c5
Peres Juliao <peresjuliao@gmail.com>
2012-03-31 17:07:54 -0300

    Alteracao classe teste

--- src/br/com/teste/ClienteTeste.java
+++ src/br/com/teste/ClienteTeste.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import br.com.bean.Cliente;
+
 public class ClienteTeste {
 
 	/**
@@ -11,9 +13,20 @@ public class ClienteTeste {
 	 */
 	public static void main(String[] args) {
 		
-		SessionFactory sessionFactory = new Configuration().buildSessionFactory();
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		
+		Cliente cliente = new Cliente();
+		cliente.setId(1);
+		cliente.setNome("Peres");
+		cliente.setSobrenome("Juliao");
+		cliente.setCpf("0314567890");
+		cliente.setSalario(1000.00);
+		cliente.setSituacao("Aposentado");
+		
+		session.save(cliente);
+		
+		session.beginTransaction().commit();
 		
 
 	}
